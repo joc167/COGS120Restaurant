e4eeec06f204a76514f9213a700c54417bae9e9b
ste <ste@stedeMacBook-Pro.local>
2018-11-01 11:46:04 -0700

    Add pages

---
+++ js0/js/introHCI.js
@@ -0,0 +1,16 @@
+// Call this function when the page loads (the jQuery "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$("#testjs").click(function(e) {
+		$('.jumbotron h1').text("Javascript is connected");
+	});
+
+	// Add any additional listeners here
+	// example: $("#div-id").click(functionToCall);
+}
